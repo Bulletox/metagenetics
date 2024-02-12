0dbd2f54be3f6b29108cc61fe93829cb1cf7472d
Bulletox <javiersernahuertas@gmail.com>
2024-02-13 00:04:11 +0100

    Actualización de estilos y corrección de errores en CSS y JavaScript

--- js/main.js
+++ js/main.js
@@ -37,3 +37,53 @@
     }
   });
 }());
+
+// -------------------------------------------------------------
+// TOAST ALERT ---------------------------------------------------------------------------------------------
+let attempts = 0;
+let tryies = 3;
+let attempts_left = 3;
+
+function unlockToastAlert() {
+  let toastUnlock = document.getElementById('unlockToast');
+  let toastText = document.getElementById('toast-body');
+  let toastUnlock_systemlocked = document.getElementById('unlockToast_systemlocked')
+  let toastText_systemlocked = document.getElementById('toast-body_systemlocked')
+  let btn_danger = document.getElementById('btn-danger')
+  var toastContainer = document.getElementById('toastContainer');
+  if (attempts_left > 1) {
+    attempts_left = (tryies - attempts)
+    toastUnlock.className = 'toast animate__animated animate__bounceInDown';
+    toastText.innerHTML = "Incorrect username or password<br>Left " + (attempts_left) + " attempts";
+    toastUnlock.style.display = ("block");
+    toastUnlock.style.position = ("fixed");
+    toastUnlock.style.zIndex = ("20");
+    toastContainer.style.display = 'flex';
+    toastContainer.style.transition = ("background-color ease 0.5s");
+    attempts++;
+
+  } else if (attempts_left === 1) {
+    toastUnlock.className = 'toast animate__animated animate__bounceInDown';
+    toastText.innerHTML = "You have reached the maximum number of login attempts.<br>The system is now locked.";
+    toastUnlock.style.display = ("block");
+    toastUnlock.style.position = ("fixed");
+    toastUnlock.style.zIndex = ("20");
+    toastContainer.style.display = 'flex';
+    toastContainer.style.transition = ("background-color ease 0.5s");
+    toastText_systemlocked.innerHTML = "Please contact an administrator to unlock your system or contact the technical department.<br><br>" +
+    "<i class='bi bi-headset'></i>  ext 666 / terminal number: 00345<br><br>" +
+    "<i class='bi bi-exclamation-circle'></i>  alert code: / 07-566";;
+    toastUnlock_systemlocked.style.display = ("block");
+    toastUnlock_systemlocked.style.position = ("fixed");
+    toastUnlock_systemlocked.style.zIndex = ("20");
+    toastUnlock_systemlocked.className = 'toast sytemblocked top-0 end-0 animate__animated animate__fadeIn animate__delay-0.3s'
+    btn_danger.style.display = "block"
+  }
+}
+
+// CLOSE ANY TOAST -------------------------------------------------------------------------------------------
+function closeToast() {
+  let toastUnlock = document.getElementById('unlockToast');
+  toastUnlock.style = "none"
+  toastContainer.style.display = 'none';
+}
