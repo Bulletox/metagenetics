6d26d1754f17c9f7535203eaf4a97eb516e5fc32
Bulletox <javiersernahuertas@gmail.com>
2024-02-13 13:13:54 +0100

    a

--- js/main.js
+++ js/main.js
@@ -45,6 +45,7 @@ let tryies = 3;
 let attempts_left = 3;
 
 function unlockToastAlert() {
+  closeToast()
   let toastUnlock = document.getElementById('unlockToast');
   let toastText = document.getElementById('toast-body');
   let toastUnlock_systemlocked = document.getElementById('unlockToast_systemlocked')
@@ -52,6 +53,7 @@ function unlockToastAlert() {
   let btn_danger = document.getElementById('btn-danger')
   var toastContainer = document.getElementById('toastContainer');
   if (attempts_left > 1) {
+
     attempts_left = (tryies - attempts)
     toastUnlock.className = 'toast animate__animated animate__bounceInDown';
     toastText.innerHTML = "Incorrect username or password<br>Left " + (attempts_left) + " attempts";
@@ -62,7 +64,7 @@ function unlockToastAlert() {
     toastContainer.style.transition = ("background-color ease 0.5s");
     attempts++;
 
-  } else if (attempts_left === 1) {
+  } else if (attempts_left = 1) {
     toastUnlock.className = 'toast animate__animated animate__bounceInDown';
     toastText.innerHTML = "You have reached the maximum number of login attempts.<br>The system is now locked.";
     toastUnlock.style.display = ("block");
@@ -78,6 +80,43 @@ function unlockToastAlert() {
     toastUnlock_systemlocked.style.zIndex = ("20");
     toastUnlock_systemlocked.className = 'toast sytemblocked top-0 end-0 animate__animated animate__fadeIn animate__delay-0.3s'
     btn_danger.style.display = "block"
+  }
+}
+
+function unlockAdmin(){
+  let toastAdmin = document.getElementById('unlockToast_administrator');
+  let usernameInput = document.getElementById('username');
+  let passwordInput = document.getElementById('password');
+  let unlockButton = document.getElementById('btn-danger');
+  let enterButton = document.getElementById('buttonfrom'); // Cambio aquí
+
+  // Desactivar inputs y botones
+  usernameInput.disabled = true;
+  passwordInput.disabled = true;
+  unlockButton.disabled = true;
+  enterButton.disabled = true; // Cambio aquí
+
+  // Mostrar el toast
+  toastAdmin.style.display = "block";
+
+  // Cerrar el toast después de un tiempo (si es necesario)
+  setTimeout(closeToast, 5000); // Cierra el toast después de 5 segundos
+}
+function checkCredentials(username, password) {
+  return username === "admin" && password === "admin";
+}
+
+function unlockadm() {
+  let username = document.getElementById('usernamead').value;
+  let password = document.getElementById('passwordad').value;
+  
+  if (checkCredentials(username, password)) {
+    // Aquí muestras el toast admin si las credenciales son correctas
+    
+  } else {
+    let toastAdmin = document.getElementById('erroradm');
+    toastAdmin.style.display = "block";
+    toastUnlock.className = 'toast animate__animated animate__bounceInDown';
   }
 }
 
